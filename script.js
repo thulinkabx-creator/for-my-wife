b43845e2dc9d3ad73a566a374eec64b93ddb03d7
thulinkabx-creator <thulinkabx@gmail.com>
2026-07-21 06:35:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -192,3 +192,20 @@ window.open(
 };
 
 }
+window.addEventListener("load", () => {
+
+setTimeout(() => {
+
+const loader = document.getElementById("loader");
+
+loader.style.opacity = "0";
+
+setTimeout(() => {
+
+loader.style.display = "none";
+
+},1000);
+
+},2000);
+
+});
