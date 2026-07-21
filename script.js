23a4f8296f8a66098f6c2f049fa8c156dad50854
thulinkabx-creator <thulinkabx@gmail.com>
2026-07-21 07:19:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -207,13 +207,11 @@ window.addEventListener("load", () => {
 
 setTimeout(() => {
 
-const loader = document.getElementById("loader");
 
 loader.style.opacity = "0";
 
 setTimeout(() => {
 
-loader.style.display = "none";
 
 },1000);
 
